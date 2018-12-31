1dad9872086f77ab3c9c8aa1516215d9509118e4
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2018-12-31 18:54:03 +0400

    Added light design

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Amiraslan Bakhshili");
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'html');
+
+app.use(express.static('assets'));
+
+// Port number 2424
+app.listen(2424);
+console.log('Server is listening...');
+
+app.get('/', function(req, res) {
+  console.log('200 Home page');
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('*', function(req, res) {
+  console.log(req.url);
+});
