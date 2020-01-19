56489296da9b254e774302eefb4625add649a8dc
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2020-01-20 00:51:47 +0200

    Small change

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,11 +2,14 @@ var helper = require('../helper');
 
 module.exports = function(app) {
 
+
   // Landing page
   app.get('/', function(req, res) {
-    res.sendFile('views/index.html', {
-      root: helper.trimDirectory(__dirname)
-    });
+    //res.sendFile('views/index.html', {
+    //  root: helper.trimDirectory(__dirname)
+    //});
+	res.sendFile('views/index.html', { root: __dirname });
+
   });
 
   // Random name api
